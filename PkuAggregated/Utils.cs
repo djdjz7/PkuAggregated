c7ae7b46b5859dadae19a1847927492b7bb3b852
djdjz7 <djdjz7@icloud.com>
2024-12-07 15:25:20 +0800

    chore(backend): stricten verification

--- PkuAggregated/Utils.cs
+++ PkuAggregated/Utils.cs
@@ -5,20 +5,18 @@ namespace PkuAggregated
 {
     public static class Utils
     {
-        public static string GenerateVerificationToken()
+        public static bool VerifyToken(string token, DateTime requestTime)
         {
-            string currentDate = DateTime.Now.ToString("yyyyMMdd");
+            if(Math.Abs((DateTime.Now - requestTime).TotalSeconds) > 20) return false;
+            string currentDate = requestTime.ToString("yyyyMMddHHmmss");
             string combinedString = currentDate + Params.TokenGeneratorSource;
-            using (SHA256 sha256 = SHA256.Create())
+            var hashBytes = SHA256.HashData(Encoding.UTF8.GetBytes(combinedString));
+            var sb = new StringBuilder();
+            foreach (byte b in hashBytes)
             {
-                var hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(combinedString));
-                var sb = new StringBuilder();
-                foreach (byte b in hashBytes)
-                {
-                    sb.Append(b.ToString("X2"));
-                }
-                return sb.ToString().Substring(0, 16);
+                sb.Append(b.ToString("X2"));
             }
+            return token.Equals(sb.ToString(), StringComparison.OrdinalIgnoreCase);
         }
     }
 }
